48984caff03463eb68a0b69b887b17d2adb21941
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-11 02:27:46 +0100

    Bump copyright year

--- classes/InfoCommand.php
+++ classes/InfoCommand.php
@@ -8,7 +8,7 @@
  * @category  CMSimple_XH
  * @package   Yanp
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2011-2015 Christoph M. Becker <http://3-magi.net/>
+ * @copyright 2011-2017 Christoph M. Becker <http://3-magi.net/>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Yanp_XH
  */
@@ -83,7 +83,7 @@ class Yanp_InfoCommand
     protected function renderLicense()
     {
         return <<<EOT
-<p>Copyright &copy; 2011-2015 <a href="http://3-magi.net/"
+<p>Copyright &copy; 2011-2017 <a href="http://3-magi.net/"
 target="_blank">Christoph M. Becker</a></p>
 <p class="yanp_license">This program is free software: you can redistribute it
 and/or modify it under the terms of the GNU General Public License as published
