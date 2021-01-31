b66c5539cf8200fe066d25ade338c0fa2d74702d
Christoph M. Becker <cmbecker69@gmx.de>
2021-01-31 14:30:45 +0100

    Add minimal unit test for some commands
    For now, we're just testing that command execution renders the desired
    template.

---
+++ tests/unit/PageDataCommandTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright 2021 Christoph M. Becker
+ *
+ * This file is part of Yanp_XH.
+ *
+ * Yanp_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Yanp_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Yanp_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Yanp;
+
+use PHPUnit\Framework\TestCase;
+
+class PageDataCommandTest extends TestCase
+{
+    public function testExecutionRendersTemplate(): void
+    {
+        $view = $this->createMock(View::class);
+        $subject = new PageDataCommand(['yanp_description' => ''], $view);
+        $view->expects($this->once())
+            ->method('render')
+            ->with($this->equalTo('pdtab'));
+        $subject->execute();
+    }
+}
---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Copyright 2021 Christoph M. Becker
+ *
+ * This file is part of Yanp_XH.
+ *
+ * Yanp_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Yanp_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Yanp_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+spl_autoload_register(function ($classname) {
+    $parts = explode('\\', $classname);
+    if ($parts[0] === 'Yanp') {
+        include_once "./classes/{$parts[1]}.php";
+    }
+});
+
+const CMSIMPLE_XH_VERSION = '1.7.0';
+const YANP_VERSION = '1pl5';
+
+function XH_helpIcon()
+{
+    return null;
+}
